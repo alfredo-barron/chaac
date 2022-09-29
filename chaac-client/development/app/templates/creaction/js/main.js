0ac6a752fdc0b9ee7f06bd6b878f833f27592521
alejandroZequeira <alejandrozequeirarosa@gmail.com>
2022-09-29 15:42:10 -0500

    correcion de errores al main.js

--- chaac-client/development/app/templates/creaction/js/main.js
+++ chaac-client/development/app/templates/creaction/js/main.js
@@ -1,27 +1,27 @@
 const lista=document.getElementById("lista");
-<<<<<<< HEAD
-=======
+
+
 const modelo=document.getElementById("modelo");
->>>>>>> 9ac30bee6934ac462e0cad59ab59cebfaaffcaa9
+
 
 Sortable.create(lista,{
 	chosenClass: "seleccion",
 	ghostClass: "fantasma",
-<<<<<<< HEAD
-	group: "lista-items"
-<<<<<<< HEAD
-=======
-=======
+
+	group: "lista-items",
+
+
 	group: {
 		name:"lista-items",
 		pull: "clone",
 		put: false
 	}
->>>>>>> e8e6f98 (modificaciones a la ramma chaac_client_2)
+	,
+	 sort: false
 });
 Sortable.create(modelo,{
 	chosenClass: "seleccion",
 	ghostClass: "fantasma",
 	group: "lista-items"
->>>>>>> 9ac30bee6934ac462e0cad59ab59cebfaaffcaa9
+
 });
