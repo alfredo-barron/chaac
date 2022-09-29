9ac30bee6934ac462e0cad59ab59cebfaaffcaa9
alejandroZequeira <alejandrozequeirarosa@gmail.com>
2022-09-29 13:13:25 -0500

    modificaciones a las capetas logs,bd del middleware

--- chaac-client/development/app/templates/creaction/js/main.js
+++ chaac-client/development/app/templates/creaction/js/main.js
@@ -1,6 +1,12 @@
 const lista=document.getElementById("lista");
+const modelo=document.getElementById("modelo");
 
 Sortable.create(lista,{
+	chosenClass: "seleccion",
+	ghostClass: "fantasma",
+	group: "lista-items"
+});
+Sortable.create(modelo,{
 	chosenClass: "seleccion",
 	ghostClass: "fantasma",
 	group: "lista-items"
