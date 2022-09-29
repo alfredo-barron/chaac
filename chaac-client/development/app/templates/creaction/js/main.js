7bb58202441b610e63a873841a6e9089902ad4b7
alejandroZequeira <alejandrozequeirarosa@gmail.com>
2022-09-29 13:28:52 -0500

    modificaciones a la ramma chaac_client_2

--- chaac-client/development/app/templates/creaction/js/main.js
+++ chaac-client/development/app/templates/creaction/js/main.js
@@ -7,9 +7,17 @@ const modelo=document.getElementById("modelo");
 Sortable.create(lista,{
 	chosenClass: "seleccion",
 	ghostClass: "fantasma",
+<<<<<<< HEAD
 	group: "lista-items"
 <<<<<<< HEAD
 =======
+=======
+	group: {
+		name:"lista-items",
+		pull: "clone",
+		put: false
+	}
+>>>>>>> e8e6f98 (modificaciones a la ramma chaac_client_2)
 });
 Sortable.create(modelo,{
 	chosenClass: "seleccion",
